3eb98d3b7845bdef4cbcdd66741af57bd6eaa344
Al Baker <baker.al@gmail.com>
2024-11-13 14:28:18 -0500

    Input config data asset

---
+++ Source/Aura/Private/Input/AuraInputConfig.cpp
@@ -0,0 +1,23 @@
+﻿// Copyright Al Learning
+
+
+#include "Input/AuraInputConfig.h"
+
+const UInputAction* UAuraInputConfig::FindAbilityInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound)
+{
+	for (auto AbilityInputAction : AbilityInputActions)
+	{
+		if (AbilityInputAction.InputTag == InputTag)
+		{
+			if (AbilityInputAction.InputAction)
+			{
+				return AbilityInputAction.InputAction;
+			}
+		}
+	}
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find abilityInputAction for InputTag [%s], on inputConfig [%s]"), *InputTag.ToString(), *GetNameSafe(this));
+	}
+	return nullptr;
+}
